72b8cabdc4fc46d14d0e4c91eea3b71a5e39fbe7
Alonso Hernández <alonsohdez14@gmail.com>
2021-08-17 16:35:44 -0500

    Add data file

---
+++ data.js
@@ -0,0 +1,54 @@
+export const weeks = [
+    {
+        number: 1,
+        title: 'the really true beginning',
+        posts: [
+            {
+                title: 'Bunny',
+                date: '18-Julio-2021',
+                url: 'https://i.imgur.com/4i40dAs.jpg'
+            },
+            {
+                title: 'Bunny',
+                date: '18-Julio-2021',
+                url: 'https://i.imgur.com/4i40dAs.jpg'
+            },
+            {
+                title: 'Bunny',
+                date: '18-Julio-2021',
+                url: 'https://i.imgur.com/4i40dAs.jpg'
+            },
+            {
+                title: 'Bunny',
+                date: '18-Julio-2021',
+                url: 'https://i.imgur.com/4i40dAs.jpg'
+            },
+        ]
+    },
+    {
+        number: 2,
+        title: 'the really true beginning 2',
+        posts: [
+            {
+                title: 'Bunny',
+                date: '18-Julio-2021',
+                url: 'https://i.imgur.com/4i40dAs.jpg'
+            },
+            {
+                title: 'Bunny',
+                date: '18-Julio-2021',
+                url: 'https://i.imgur.com/4i40dAs.jpg'
+            },
+            {
+                title: 'Bunny',
+                date: '18-Julio-2021',
+                url: 'https://i.imgur.com/4i40dAs.jpg'
+            },
+            {
+                title: 'Bunny',
+                date: '18-Julio-2021',
+                url: 'https://i.imgur.com/4i40dAs.jpg'
+            },
+        ]
+    }
+]
--- src/views/MonToFri/index.jsx
+++ src/views/MonToFri/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Hero from "../../components/monToFri/Hero";
 import Week from "../../components/monToFri/Week";
 
-import {weeks} from "../../../../data";
+import {weeks} from "../../../data";
 
 const MonToFri = () => {
     const numberWeeks = weeks.length
