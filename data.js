c6b1679b7c2c2c6b185b6fb67d2e95bcb0e1648d
Jesús Hernández <jesus10.hn@gmail.com>
2021-09-05 16:15:10 -0500

    Fix PostDetails style

--- data.js
+++ data.js
@@ -1,290 +1,321 @@
 export const weeks = [
-	//objeto semana 1
-	{
-		//llave: valor
-		number: 1,
-		title: 'the really true beginning',
-		posts: [
-			{
-				title: '🐰🐰',
-				date: '28-Jun-2021',
-				url: 'https://i.imgur.com/4i40dAs.jpg',
-				behind: 'https://i.imgur.com/a6FUhdV.jpg',
-				resolution: '2400 x 3000',
-				size: '11.1',
-			},
-			{
-				title: 'THE QUESTION',
-				date: '29-Jun-2021',
-				url: 'https://i.imgur.com/hj9ng5K.jpg',
-			},
-			{
-				title: 'SHIP',
-				date: '30-Jun-2021',
-				url: 'https://i.imgur.com/tk7OMKR.jpg',
-			},
-			{
-				title: 'LOST',
-				date: '01-Jul-2021',
-				url: 'https://i.imgur.com/JcshEjX.jpg',
-			},
-			{
-				title: '🥶🥶🥶',
-				date: '02-Jul-2021',
-				url: 'https://i.imgur.com/SnpboS2.jpg',
-			},
-		],
-	},
-	{
-		number: 2,
-		title: '',
-		posts: [
-			{
-				title: 'PRISMA FULL',
-				date: '05-Jul-2021',
-				url: 'https://i.imgur.com/ssrquCb.jpg',
-			},
-			{
-				title: 'Thank you for your purchase! 🤝🏻',
-				date: '06-Jul-2021',
-				url: 'https://i.imgur.com/9Look2H.jpg',
-			},
-			{
-				title: 'SH*T HAPPENS 💩',
-				date: '07-Jul-2021',
-				url: 'https://i.imgur.com/7Eufx4L.jpg',
-			},
-			{
-				title: 'SUS',
-				date: '08-Jul-2021',
-				url: 'https://i.imgur.com/wIFUEVe.jpg',
-			},
-			{
-				title: 'BANANAFETT',
-				date: '09-Jul-2021',
-				url: 'https://i.imgur.com/aJO467M.jpg',
-			},
-		],
-	},
-	{
-		number: 3,
-		title: '',
-		posts: [
-			{
-				title: 'SWEET DEAD',
-				date: '12-Jul-2021',
-				url: 'https://i.imgur.com/CnbCPle.jpg',
-			},
-			{
-				title: '🌞',
-				date: '13-Jul-2021',
-				url: 'https://i.imgur.com/ClWROSw.jpg',
-			},
-			{
-				title: 'CALM WATERS',
-				date: '14-Jul-2021',
-				url: 'https://i.imgur.com/JKI7viH.jpg',
-			},
-			{
-				title: 'FRENCH BURGER 🍔🍟',
-				date: '15-Jul-2021',
-				url: 'https://i.imgur.com/eLohRJV.jpg',
-			},
-			{
-				title: 'ROLL',
-				date: '16-Jul-2021',
-				url: 'https://i.imgur.com/MD3RDIK.jpg',
-			},
-		],
-	},
-	{
-		number: 4,
-		title: '',
-		posts: [
-			{
-				title: 'VERY NEW SOUNDMOJIS (AND GOATMOJIS! 🐐)',
-				date: '19-Jul-2021',
-				url: 'https://i.imgur.com/UmW9Z16.jpg',
-			},
-			{
-				title: 'FINDING SIGNALS',
-				date: '20-Jul-2021',
-				url: 'https://i.imgur.com/TEk1URQ.jpg',
-			},
-			{
-				title: 'KICK IT UP',
-				date: '21-Jul-2021',
-				url: 'https://i.imgur.com/JbfA40d.jpg',
-			},
-			{
-				title: 'URBAN',
-				date: '22-Jul-2021',
-				url: 'https://i.imgur.com/ppMBB0J.jpg',
-			},
-			{
-				title: 'DAY OFF',
-				date: '23-Jul-2021',
-				url: 'https://i.imgur.com/0jsGRAA.jpg',
-			},
-		],
-	},
-	{
-		number: 5,
-		title: '',
-		posts: [
-			{
-				title: 'LAST MISSION',
-				date: '26-Jul-2021',
-				url: 'https://i.imgur.com/lHq86DV.jpg',
-			},
-			{
-				title: 'ABDUZCAN',
-				date: '27-Jul-2021',
-				url: 'https://i.imgur.com/Ks1nHma.jpg',
-			},
-			{
-				title: 'LIFE GOES ON...',
-				date: '28-Jul-2021',
-				url: 'https://i.imgur.com/OqRUcbX.jpg',
-			},
-			{
-				title: 'INACCESIBLE',
-				date: '29-Jul-2021',
-				url: 'https://i.imgur.com/1ehuKXQ.jpg',
-			},
-			{
-				title: 'LET IT GO',
-				date: '30-Jul-2021',
-				url: 'https://i.imgur.com/NAC9Lsg.jpg',
-			},
-		],
-	},
-	{
-		number: 6,
-		title: '',
-		posts: [
-			{
-				title: 'FORSAKEN',
-				date: '02-Aug-2021',
-				url: 'https://i.imgur.com/F5BlldW.jpg',
-			},
-			{
-				title: 'BREAKAGE',
-				date: '03-Aug-2021',
-				url: 'https://i.imgur.com/aoT5mRZ.jpg',
-			},
-			{
-				title: 'REDECORATE',
-				date: '04-Aug-2021',
-				url: 'https://i.imgur.com/tcXRomd.jpg',
-			},
-			{
-				title: 'REPLACING',
-				date: '05-Aug-2021',
-				url: 'https://i.imgur.com/B2SSYJI.jpg',
-			},
-			{
-				title: 'IN A VOYAGE',
-				date: '06-Aug-2021',
-				url: 'https://i.imgur.com/QTZp7d9.jpg',
-				behind: 'hola mundo',
-			},
-		],
-	},
-	{
-		number: 7,
-		title: '',
-		posts: [
-			{
-				title: "AXOLOTL'S LUNCH",
-				date: '09-Aug-2021',
-				url: 'https://i.imgur.com/OsMm0a1.jpg',
-			},
-			{
-				title: 'PICKING FLOWERS',
-				date: '10-Aug-2021',
-				url: 'https://i.imgur.com/62CHe8x.png',
-			},
-			{
-				title: 'METARABBITMASK',
-				date: '11-Aug-2021',
-				url: 'https://i.imgur.com/L5qKFWe.png',
-			},
-			{
-				title: 'DEEP',
-				date: '12-Aug-2021',
-				url: 'https://i.imgur.com/UUtmCoP.png',
-			},
-			{
-				title: 'CONTAINER',
-				date: '13-Aug-2021',
-				url: 'https://i.imgur.com/iEvOuH9.png',
-			},
-		],
-	},
-	{
-		number: 8,
-		title: '',
-		posts: [
-			{
-				title: 'BLAST',
-				date: '16-Aug-2021',
-				url: 'https://i.imgur.com/G3mVtcz.png',
-			},
-			{
-				title: 'ARCADE',
-				date: '17-Aug-2021',
-				url: 'https://i.imgur.com/5w2SKkO.png',
-				behind: 'hola mundo',
-			},
-			{
-				title: 'WHEN PIGS FLY',
-				date: '18-Aug-2021',
-				url: 'https://i.imgur.com/tcKWtFe.png',
-				behind: 'hola pig',
-			},
-			{
-				title: 'GAME BOT',
-				date: '19-Aug-2021',
-				url: 'https://i.imgur.com/rbAEsuo.png',
-				behind: 'hola mundo',
-			},
-			{
-				title: 'LOCKED',
-				date: '20-Aug-2021',
-				url: 'https://i.imgur.com/lbPiJdM.png',
-			},
-		],
-	},
-	{
-		number: 9,
-		title: '',
-		posts: [
-			{
-				title: 'FROG DOMINATION 🐸',
-				date: '23-Aug-2021',
-				url: 'https://i.imgur.com/KKh5lRv.png',
-			},
-			{
-				title: 'HEAVY MACHINE GUN!',
-				date: '24-Aug-2021',
-				url: 'https://i.imgur.com/EkNmLIb.png',
-			},
-			{
-				title: 'ENCAPSULATED',
-				date: '25-Aug-2021',
-				url: 'https://i.imgur.com/hfzqbUs.png',
-			},
-			{
-				title: 'IMMUNITY',
-				date: '26-Aug-2021',
-				url: 'https://i.imgur.com/w8z2oVk.png',
-			},
-			{
-				title: 'VUELO',
-				date: '27-Aug-2021',
-				url: 'https://i.imgur.com/PHAhskT.png',
-			},
-		],
-	},
+  //objeto semana 1
+  {
+    //llave: valor
+    number: 1,
+    title: "the really true beginning",
+    posts: [
+      {
+        title: "🐰🐰",
+        date: "28-Jun-2021",
+        url: "https://i.imgur.com/4i40dAs.jpg",
+        behind: "https://i.imgur.com/a6FUhdV.jpg",
+        resolution: "2400 x 3000",
+        size: "11.1",
+      },
+      {
+        title: "THE QUESTION",
+        date: "29-Jun-2021",
+        url: "https://i.imgur.com/hj9ng5K.jpg",
+      },
+      {
+        title: "SHIP",
+        date: "30-Jun-2021",
+        url: "https://i.imgur.com/tk7OMKR.jpg",
+      },
+      {
+        title: "LOST",
+        date: "01-Jul-2021",
+        url: "https://i.imgur.com/JcshEjX.jpg",
+      },
+      {
+        title: "🥶🥶🥶",
+        date: "02-Jul-2021",
+        url: "https://i.imgur.com/SnpboS2.jpg",
+      },
+    ],
+  },
+  {
+    number: 2,
+    title: "",
+    posts: [
+      {
+        title: "PRISMA FULL",
+        date: "05-Jul-2021",
+        url: "https://i.imgur.com/ssrquCb.jpg",
+      },
+      {
+        title: "Thank you for your purchase! 🤝🏻",
+        date: "06-Jul-2021",
+        url: "https://i.imgur.com/9Look2H.jpg",
+      },
+      {
+        title: "SH*T HAPPENS 💩",
+        date: "07-Jul-2021",
+        url: "https://i.imgur.com/7Eufx4L.jpg",
+      },
+      {
+        title: "SUS",
+        date: "08-Jul-2021",
+        url: "https://i.imgur.com/wIFUEVe.jpg",
+      },
+      {
+        title: "BANANAFETT",
+        date: "09-Jul-2021",
+        url: "https://i.imgur.com/aJO467M.jpg",
+      },
+    ],
+  },
+  {
+    number: 3,
+    title: "",
+    posts: [
+      {
+        title: "SWEET DEAD",
+        date: "12-Jul-2021",
+        url: "https://i.imgur.com/CnbCPle.jpg",
+      },
+      {
+        title: "🌞",
+        date: "13-Jul-2021",
+        url: "https://i.imgur.com/ClWROSw.jpg",
+      },
+      {
+        title: "CALM WATERS",
+        date: "14-Jul-2021",
+        url: "https://i.imgur.com/JKI7viH.jpg",
+      },
+      {
+        title: "FRENCH BURGER 🍔🍟",
+        date: "15-Jul-2021",
+        url: "https://i.imgur.com/eLohRJV.jpg",
+      },
+      {
+        title: "ROLL",
+        date: "16-Jul-2021",
+        url: "https://i.imgur.com/MD3RDIK.jpg",
+      },
+    ],
+  },
+  {
+    number: 4,
+    title: "",
+    posts: [
+      {
+        title: "VERY NEW SOUNDMOJIS (AND GOATMOJIS! 🐐)",
+        date: "19-Jul-2021",
+        url: "https://i.imgur.com/UmW9Z16.jpg",
+      },
+      {
+        title: "FINDING SIGNALS",
+        date: "20-Jul-2021",
+        url: "https://i.imgur.com/TEk1URQ.jpg",
+      },
+      {
+        title: "KICK IT UP",
+        date: "21-Jul-2021",
+        url: "https://i.imgur.com/JbfA40d.jpg",
+      },
+      {
+        title: "URBAN",
+        date: "22-Jul-2021",
+        url: "https://i.imgur.com/ppMBB0J.jpg",
+      },
+      {
+        title: "DAY OFF",
+        date: "23-Jul-2021",
+        url: "https://i.imgur.com/0jsGRAA.jpg",
+      },
+    ],
+  },
+  {
+    number: 5,
+    title: "",
+    posts: [
+      {
+        title: "LAST MISSION",
+        date: "26-Jul-2021",
+        url: "https://i.imgur.com/lHq86DV.jpg",
+      },
+      {
+        title: "ABDUZCAN",
+        date: "27-Jul-2021",
+        url: "https://i.imgur.com/Ks1nHma.jpg",
+      },
+      {
+        title: "LIFE GOES ON...",
+        date: "28-Jul-2021",
+        url: "https://i.imgur.com/OqRUcbX.jpg",
+      },
+      {
+        title: "INACCESIBLE",
+        date: "29-Jul-2021",
+        url: "https://i.imgur.com/1ehuKXQ.jpg",
+      },
+      {
+        title: "LET IT GO",
+        date: "30-Jul-2021",
+        url: "https://i.imgur.com/NAC9Lsg.jpg",
+      },
+    ],
+  },
+  {
+    number: 6,
+    title: "",
+    posts: [
+      {
+        title: "FORSAKEN",
+        date: "02-Aug-2021",
+        url: "https://i.imgur.com/F5BlldW.jpg",
+      },
+      {
+        title: "BREAKAGE",
+        date: "03-Aug-2021",
+        url: "https://i.imgur.com/aoT5mRZ.jpg",
+      },
+      {
+        title: "REDECORATE",
+        date: "04-Aug-2021",
+        url: "https://i.imgur.com/tcXRomd.jpg",
+      },
+      {
+        title: "REPLACING",
+        date: "05-Aug-2021",
+        url: "https://i.imgur.com/B2SSYJI.jpg",
+      },
+      {
+        title: "IN A VOYAGE",
+        date: "06-Aug-2021",
+        url: "https://i.imgur.com/QTZp7d9.jpg",
+        behind: "hola mundo",
+      },
+    ],
+  },
+  {
+    number: 7,
+    title: "",
+    posts: [
+      {
+        title: "AXOLOTL'S LUNCH",
+        date: "09-Aug-2021",
+        url: "https://i.imgur.com/OsMm0a1.jpg",
+      },
+      {
+        title: "PICKING FLOWERS",
+        date: "10-Aug-2021",
+        url: "https://i.imgur.com/62CHe8x.png",
+      },
+      {
+        title: "METARABBITMASK",
+        date: "11-Aug-2021",
+        url: "https://i.imgur.com/L5qKFWe.png",
+      },
+      {
+        title: "DEEP",
+        date: "12-Aug-2021",
+        url: "https://i.imgur.com/UUtmCoP.png",
+      },
+      {
+        title: "CONTAINER",
+        date: "13-Aug-2021",
+        url: "https://i.imgur.com/iEvOuH9.png",
+      },
+    ],
+  },
+  {
+    number: 8,
+    title: "",
+    posts: [
+      {
+        title: "BLAST",
+        date: "16-Aug-2021",
+        url: "https://i.imgur.com/G3mVtcz.png",
+      },
+      {
+        title: "ARCADE",
+        date: "17-Aug-2021",
+        url: "https://i.imgur.com/5w2SKkO.png",
+        behind: "hola mundo",
+      },
+      {
+        title: "WHEN PIGS FLY",
+        date: "18-Aug-2021",
+        url: "https://i.imgur.com/tcKWtFe.png",
+        behind: "hola pig",
+      },
+      {
+        title: "GAME BOT",
+        date: "19-Aug-2021",
+        url: "https://i.imgur.com/rbAEsuo.png",
+        behind: "hola mundo",
+      },
+      {
+        title: "LOCKED",
+        date: "20-Aug-2021",
+        url: "https://i.imgur.com/lbPiJdM.png",
+      },
+    ],
+  },
+  {
+    number: 9,
+    title: "",
+    posts: [
+      {
+        title: "FROG DOMINATION 🐸",
+        date: "23-Aug-2021",
+        url: "https://i.imgur.com/KKh5lRv.png",
+      },
+      {
+        title: "HEAVY MACHINE GUN!",
+        date: "24-Aug-2021",
+        url: "https://i.imgur.com/EkNmLIb.png",
+      },
+      {
+        title: "ENCAPSULATED",
+        date: "25-Aug-2021",
+        url: "https://i.imgur.com/hfzqbUs.png",
+      },
+      {
+        title: "IMMUNITY",
+        date: "26-Aug-2021",
+        url: "https://i.imgur.com/w8z2oVk.png",
+      },
+      {
+        title: "VUELO",
+        date: "27-Aug-2021",
+        url: "https://i.imgur.com/PHAhskT.png",
+      },
+    ],
+  },
+  {
+    number: 10,
+    title: "",
+    posts: [
+      {
+        title: "ROTTEN EGG 🥚",
+        date: "30-Aug-2021",
+        url: "https://i.imgur.com/AMQjgHY.png",
+      },
+      {
+        title: "Flows",
+        date: "31-Aug-2021",
+        url: "https://i.imgur.com/0IU8JVq.png",
+      },
+      {
+        title: "⚛️.jsx",
+        date: "01-Sep-2021",
+        url: "https://i.imgur.com/td7mhcL.png",
+      },
+      {
+        title: "FULL MAIL",
+        date: "02-Sep-2021",
+        url: "https://i.imgur.com/4VLwWfH.png",
+      },
+      {
+        title: "HULL",
+        date: "03-Sep-2021",
+        url: "https://i.imgur.com/hMQ5qR3.png",
+      },
+    ],
+  },
 ];
--- data_wallpapers.js
+++ data_wallpapers.js
@@ -1,33 +1,27 @@
 export const wallpapers = [
-    {
-        id: 1,
-        url: 'https://i.imgur.com/onVtToF.png',
-        date: '29-Aug-2020',
-        description: '"spaceman"'
-    },{
-        id: 2,
-        url: 'https://i.imgur.com/onVtToF.png',
-        date: '03-Sep-2020',
-        description: '"IMAGINATION"'
-    }, {
-        id: 3,
-        url: 'https://i.imgur.com/onVtToF.png',
-        date: '04-Sep-2020',
-        description: '"AppleHead"'
-    },{
-        id: 4,
-        url: 'https://i.imgur.com/onVtToF.png',
-        date: '08-Sep-2020',
-        description: '"Inner Beauty"'
-    },{
-        id: 5,
-        url: 'https://i.imgur.com/onVtToF.png',
-        date: '29-Aug-2020',
-        description: '"@pame_nyan"'
-    }, {
-        id: 6,
-        url: 'https://i.imgur.com/onVtToF.png',
-        date: '03-Sep-2020',
-        description: '"IMAGINATION"'
-    },
-]
+  {
+    id: 1,
+    url: "https://i.imgur.com/onVtToF.png",
+    resolution: "1920 x 1080 px",
+    size: "779 KB",
+  },
+  {
+    id: 2,
+    url: "https://i.imgur.com/06a0v7O.png",
+    resolution: "1920 x 1080 px",
+    size: "1.48 MB",
+  },
+  {
+    id: 3,
+    url: "https://i.imgur.com/wXMTixZ.png",
+
+    resolution: "2560 x 1440 px",
+    size: "2.38 MB",
+  },
+  {
+    id: 4,
+    url: "https://i.imgur.com/XAuGL0u.png",
+    resolution: "2560 x 1440 px",
+    size: "2.24 MB",
+  },
+];
